ded1d4c4b5e92734b6fbeb3980930819164fbd8f
tutonjason <codingwithjson@gmail.com>
2024-03-27 23:37:31 +0800

    configure shadcdn to new fresh installed vue via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
